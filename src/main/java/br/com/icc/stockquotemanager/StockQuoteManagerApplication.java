dd77470da4e21d64de5e6c587f85f44f298dcbbc
Aguinaldo Feliciano <aguinaldojunior@gec.inatel.br>
2021-05-19 20:55:12 -0300

    Fix add method and add some logs

--- src/main/java/br/com/icc/stockquotemanager/StockQuoteManagerApplication.java
+++ src/main/java/br/com/icc/stockquotemanager/StockQuoteManagerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/")
 @SpringBootApplication
+
 public class StockQuoteManagerApplication {
 
 	public static void main(String[] args) {
